c0948323dcf7ef10d96ba0d2a5839949ada459cc
pederan <peder1976@gmail.com>
2020-12-10 14:52:17 +0100

    changed logic, only add cache control if not exists

--- plugin.php
+++ plugin.php
@@ -17,18 +17,11 @@ namespace DSS\Plugin\Set_Cache;
 \add_filter( 'wp_headers', __NAMESPACE__ . '\\on_wp_headers', 10, 1 );
 
 function on_wp_headers( array $headers ) : array {
-	// check if cache control is set in the header.
-	if ( isset( $headers['Cache-Control'] ) && \is_string( $headers['Cache-Control'] ) ) {
-		$current_cache_control = \array_map( 'trim', \explode( ',', $headers['Cache-Control'] ) );
-
-		// check for no caching.
-		if ( \in_array( 'no-cache', $current_cache_control, true ) ) {
-			// Set default caching to 300 seconds / 5 minutes.
-			$seconds_to_cache         = \apply_filters( 'dss/nettsteder/plugin/set_cache/seconds_to_cache', 300 );
-			$expires                  = \gmdate( 'D, d M Y H:i:s', \time() + $seconds_to_cache ) . ' GMT';
-			$headers['Expires']       = $expires;
-			$headers['Cache-Control'] = 'max-age=' . $seconds_to_cache;
-		}
+	// check if cache control is set in the header and if not add it.
+	if ( false === \array_key_exists( 'Cache-Control', $headers ) ) {
+		// Set default caching to 300 seconds / 5 minutes. Can be changed using the filter.
+		$seconds_to_cache         = \apply_filters( 'dss/nettsteder/plugin/set_cache/seconds_to_cache', 300 );
+		$headers['Cache-Control'] = 'public, max-age=' . $seconds_to_cache;
 	}
 
 	return $headers;
